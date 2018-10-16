9efc430e214fe272e18829f70e8276c4204a3f1c
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-16 11:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function takeANumber(people, name) {
     people.push (name)
-<<<<<<< HEAD
     return `Welcome, ${name}. You are number ${people.length} in line.`
 }
 
@@ -13,7 +12,5 @@ function nowServing(katzDeliLine) {
    else if (katzDeliLine.length ===  0){
      return "There is nobody waiting to be served!"
    }
-=======
     return `Welcome, ${name[i]}. You are number ${people[i++]} in line.`
->>>>>>> 3dda8889dfcafaab09a446a234ffef8dee2f0bf0
 }
