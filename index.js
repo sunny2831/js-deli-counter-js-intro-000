0d48e4d6a66765f1c20bd66e992c96d54697e39f
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-16 11:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 function currentLine(line) {
   if (line.length > 0){
     let name = line[0]
-    return (`The line is currently ${number}.${name}`)
+    return (`The line is currently [i++]. ${name}`)
   }
   else if (line.length === 0) {
     return "The line is currently empty."
