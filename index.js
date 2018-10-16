549681903f8b59ceeaf72ec3c343bfb124673522
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-16 12:40:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function currentLine(line){
   if (line.length === 0){
       return("The line is currently empty.");
   }
-  var lineLength = [];
-  for (var i = 1; i <= katzDeliLine.length; i++){
-      lineLength.push(`${i}. ${line[i-1]}`);
+  var queue = [];
+  for (var i = 1; i <= line.length; i++){
+      queue.push(`${i}. ${line[i-1]}`);
   }
-  return (`The line is currently: ${linelength}`);
+  return (`The line is currently: ${queue}`);
 }
