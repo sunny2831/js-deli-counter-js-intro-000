8a31dbb26c6e4b704681287dce9722056aee2fe4
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-16 12:36:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ function nowServing(katzDeliLine) {
     return `Welcome, ${name[i]}. You are number ${people[i++]} in line.`
 }
 
-function currentLine(line) {
-  if (line.length > 0){
-    let name = line[0]
-    return (`The line is currently [i]. ${name}`)
+function currentLine(line){
+  if (line.length === 0){
+      return("The line is currently empty.");
   }
-  else if (line.length === 0) {
-    return "The line is currently empty."
+  var localCurrentLine = [];
+  for (var i = 1; i <= katzDeliLine.length; i++){
+      localCurrentLine.push(`${i}. ${katzDeliLine[i-1]}`);
   }
+  return (`The line is currently: ${localCurrentLine}`);
 }
